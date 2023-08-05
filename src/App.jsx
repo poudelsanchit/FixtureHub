0cb8b292f804353b086660ed2b42cead07d97332
Sanchit Poudel <snchtpdl12@gmail.com>
2023-08-05 19:59:18 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl font-Chakra'>Hweyt</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    screens: {
+      's': '320px', //small mobile
+      'm': '375px', //small mobile
+      'l': '425px', //large mobile
+      'sm': '640px', //small Tablet
+      'md': '768px	', // tablet
+      '2md': '894px',
+      'lg': '1024px', //laptop
+      'xl': '1280px	', //large laptop
+      '2xl': '1536px	', //large screens
+    },
+    colors:
+    {
+      'navbar-bg': '#121212',
+      'dark-bg': '#0b090a',
+      'secondary-bg': '#393E46',
+      'primary-text': '#ffffff',
+      'secondary-text': '#EEEEEE'
+
+    },
+    extend: {},
+    fontFamily: {
+      Montserrat: ['Montserrat', 'sans-serif'],
+      Roboto: ['Roboto Mono', 'monospace'],
+      Chakra: ['Chakra Petch', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
